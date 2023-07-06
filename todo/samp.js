e92d41aae795feac875083367eeebcc00d112440
jhansiKaranam64 <jhansirvc66@gmail.com>
2023-07-06 21:10:29 +0530

    Merged update

--- todo/samp.js
+++ todo/samp.js
@@ -1,5 +1,10 @@
-const items = document.getElementsByClassName('list-group-item');
+/*const items = document.getElementsByClassName('list-group-item');
 items[1].style.backgroundColor='green';
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight="bold";
+}*/
+
+const items = document.getElementsByTagName('li');
 for(var i=0;i<items.length;i++){
     items[i].style.fontWeight="bold";
 }
