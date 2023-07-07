fc9c6d409593dbe643b3843c779078cc49c418f8
jhansiKaranam64 <jhansirvc66@gmail.com>
2023-07-07 19:50:26 +0530

    events

--- samp.js
+++ samp.js
@@ -29,11 +29,18 @@ for(var i=0;i<odd.length;i++){
 
 // TRAVERSING THE DOM
 
+/* var itemList = document.querySelector('#items');
+//parentNode
+itemList.parentNode.style.backgroundColor = '#f4f4f4';  //id=main is parentNode
+console.log(itemList.parentNode.parentNode); //id=container
+console.log(itemList.parentNode.parentNode.parentNode); //body
+=======
 var itemList = document.querySelector('#items');
 //parentNode
 itemList.parentNode.style.backgroundColor = '#f4f4f4';  //id=main is parentNode
 itemList.parentNode //id=container
 itemList.parentNode.parentNode //body
+>>>>>>> 9775425b4e2a774f8f639db239e2b7f9daa9d812
 
 //parentElement
 itemList.parentElement.style.backgroundColor = '#f4f4f4';
@@ -88,4 +95,59 @@ head.insertBefore(newDiv,h1);
 var item1 = document.querySelector("#items li:first-child");
 item1.parentNode.insertBefore(newDiv, item1);
 console.log(newDiv);
+
+*/
+
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+
+
+// form submit event
+form.addEventListener('submit',addItem);
+// Delete Event
+itemList.addEventListener('click',removeItem);
+
+
+
+//addItem function
+function addItem(e){
+    e.preventDefault();
+    // get input value
+    var newItem = document.getElementById('item').value;
+
+    // create new li element
+    var li = document.createElement('li');
+    //add class
+    li.className = 'list-group-item';
+    //add text node with input value
+    li.appendChild(document.createTextNode(newItem));
+
+    //create delete button
+    var deleteBtn = document.createElement('button');
+    //Add classes to delete button
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    deleteBtn.appendChild(document.createTextNode('X'));
+    //append button to li
+    li.appendChild(deleteBtn);
+
+    // create edit button
+    var editBtn = document.createElement('button');
+    editBtn.className = 'btn btn-primary btn-sm float-right edit';
+    editBtn.appendChild(document.createTextNode('edit'));
+    li.appendChild(editBtn);
+    //append li to list
+    itemList.appendChild(li);
+}
+
+// removeItem function
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are u sure?')){
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
+
+
 
---
+++ todo/1/sample.js
@@ -0,0 +1,73 @@
+//manuplating the DOM
+const ul=document.querySelector('.items');
+ul.firstElementChild.textContent='Hello';
+ul.children[1].innerText='Hii';
+ul.lastElementChild.innerHTML='<h1>What</h1';
+
+//Events
+const btn=document.querySelector('.btn');
+//onclick
+btn.addEventListener('click',e=>{
+    e.preventDefault();
+    document.getElementById('my-form').style.background="#ccc";
+    document.querySelector('body').classList.add('bg-dark');
+    ul.lastElementChild.innerHTML='<h1>changed</h1>';
+
+});
+//mouseout
+btn.addEventListener('mouseout',e=>{
+    e.preventDefault();
+    document.getElementById('my-form').style.background='purple';
+    document.querySelector('body').classList.add('bg-light');
+    ul.children[1].innerHTML='<h1>cool</h1>';
+
+});
+//mouseover
+btn.addEventListener('mouseover',e=>{
+    e.preventDefault();
+    document.getElementById('my-form').style.background='green';
+    document.querySelector('body').classList.add('bg-white');
+    ul.firstElementChild.innerHTML='<h1>over</h1>';
+});
+
+//Keyboard event
+//to display name after submitting
+const namesInput=document.querySelector('#name');
+nameInput.addEventListener('input',e=>{
+    document.querySelector('.container').append(namesInput.value);
+});
+
+/*const emailInput=document.querySelector('#email');
+emailInput.addEventListener('input',e=>{
+    document.querySelector('.container').append(emailInput.value);
+});*/
+
+//user form script
+//put DOM values into variables
+const myForm=document.querySelector('#my-form');
+const nameInput=document.querySelector('#name');
+const emailInput=document.querySelector('#email');
+const msg=document.querySelector('.msg');
+const userList=document.querySelector('#users');
+
+//Listen for form submit
+myForm.addEventListener('submit',onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+    if(nameInput.value===''||emailInput.value===''){
+        msg.classList.add('error');
+        msg.innerHTML='Please enter all fields';
+
+        //remove error after 3 sec
+       // setTimeout(()=>msg.remove(),3000);
+    }else{
+        const li=document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value}:${emailInput.value}`));
+        userList.appendChild(li);
+
+        //clear fields
+        nameInput.value='';
+        emailInput.value='';
+    }
+}
