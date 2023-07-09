8493c4093d1a2652e3e71e68fb609b9efdaaa71c
jhansiKaranam64 <jhansirvc66@gmail.com>
2023-07-09 09:50:46 +0530

    delete

--- Book/script.js
+++ Book/script.js
@@ -2,8 +2,11 @@ form = document.getElementById('my-form');
 itemList = document.getElementById('items');
 const msg=document.querySelector('.msg');
 
-form.addEventListener('submit',addItem);
 
+form.addEventListener('submit',addItem);
+itemList.addEventListener('click',removeItem);
+
+// Add details
 function addItem(e){
     e.preventDefault();
 
@@ -20,24 +23,40 @@ function addItem(e){
         setTimeout(()=>msg.remove(),3000);
     }else{
         var li =document.createElement('li');
+        var button = document.createElement('button');
+        button.className ="btn btn-danger btn-sm float-right delete";
+        button.appendChild(document.createTextNode('Delete'));
         li.className = "list-group-item";
         li.appendChild(document.createTextNode(newName.value+": "+newEmail.value+": "+newPhone.value));
+        li.appendChild(button);
         itemList.appendChild(li);
 
-        var storedData = localStorage.getItem('userDetails');
-        var userDetails = storedData ? JSON.parse(storedData):[];
+        var storedData = localStorage.getItem(newEmail.value);
+        //var userDetails = storedData ? JSON.parse(storedData):[];
         var newUser={
             name : newName.value,
             email : newEmail.value,
             phone : newPhone.value
         }
-        userDetails.push(newUser);
-        localStorage.setItem('userDetails',JSON.stringify(userDetails));
-        console.log(localStorage.getItem('userDetails'));
+        //userDetails.push(newUser);
+        localStorage.setItem(newEmail.value,JSON.stringify(newUser));
 
         //clear fields
         newName.value='';
         newEmail.value='';
         newPhone.value='';
+    }
+}
+
+// remove deatils from UI and localStorage
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are u sure?')){
+            var li = e.target.parentElement;
+            var itemLis = li.textContent.split(':');
+            var email = itemLis[1].trim();
+            localStorage.removeItem(email);
+            itemList.removeChild(li);
+        }
     }
 }
